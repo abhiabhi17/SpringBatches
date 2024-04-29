42c48f5dca442bb3dac8190c4004a4962dce2bbe
abhiabhi17 <50611304+abhiabhi17@users.noreply.github.com>
2024-04-29 22:21:18 +0530

    Update CustomerProcessor.java

--- src/main/java/com/example/springbatches/config/CustomerProcessor.java
+++ src/main/java/com/example/springbatches/config/CustomerProcessor.java
@@ -8,7 +8,7 @@ import org.springframework.batch.item.ItemProcessor;
 // Wtrite the object as customer (Inbound and outbound))
 public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
 
-
+//messgae in git website
     @Override
     public Customer process(Customer customer) throws Exception {
 //        if(customer.getCountry().equals("United States")) {
