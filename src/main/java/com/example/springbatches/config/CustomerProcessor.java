2186056c92e9fc56df243ed317dc80abfdb034f8
Abhi <pavadi.abhishek@gmail.com>
2024-04-30 16:23:53 +0530

    message

--- src/main/java/com/example/springbatches/config/CustomerProcessor.java
+++ src/main/java/com/example/springbatches/config/CustomerProcessor.java
@@ -9,7 +9,7 @@ import org.springframework.batch.item.ItemProcessor;
 public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
 
 
-
+    //this is from master
     @Override
     public Customer process(Customer customer) throws Exception {
 //        if(customer.getCountry().equals("United States")) {
